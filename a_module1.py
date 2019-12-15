ce0421d3e76efbd60454cd1a0a0b77979f248136
Vishalkaware <vishalkaware22@gmail.com>
2019-12-15 19:24:32 +0530

    new data from new learning

---
+++ a_module1.py
@@ -0,0 +1,11 @@
+a_num=10
+a_str="python"
+a_list=[10,20,30,40]
+a_float=10.45
+def square(x):
+    return x ** 2
+def fibo(n):
+    a,b=0,1
+    while b<n:
+        print(b,end=" ")
+        a,b=b,a+b
